4cd30ec0850b0eb76a0be9cb7b841704f74702ae
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-06-04 22:48:20 +0530

    first commit

---
+++ 03_typeconversion.js
@@ -0,0 +1,36 @@
+let x = "33";
+// console.log(typeof x);
+let toint = Number(x);
+// console.log(typeof toint);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+// flase => 0
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "uttkarsh" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/* in short to conver one type to another just use the typename 
+with 1st letter capital as seen above*/
+
+// *************************************OPERATIONS*************************************//
+
+let str1 = "hello";
+let str2 = " uttkarsh";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
