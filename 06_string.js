59a2e6c08878420e2ff268f476e2266f62c4769f
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-06-05 21:48:36 +0530

    Added 06

---
+++ 06_string.js
@@ -0,0 +1,11 @@
+const name = "uttkarsh"
+const repoCount = 12;
+
+// 
+
+console.log(`My name is ${name} and the repo count is ${repoCount}`);
+
+let gameName = new String('Elden Ring'); // another way to declaring strings using objects
+
+// now we must revise all the different methods of the String class
+
