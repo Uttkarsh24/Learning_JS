e4da51c45ebbdefdf35f57764e7981136e2edd9c
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-06-05 16:41:55 +0530

    Added 05

--- 04_comparisons.js
+++ 04_comparisons.js
@@ -5,5 +5,11 @@
 // console.log(2 == 1);
 // console.log(2 != 1);
 
-console.log();
 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
