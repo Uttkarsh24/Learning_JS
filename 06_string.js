fa88dedfe659dee932776f20acbef02e9a8b1225
Uttkarsh24 <uttkarshsingh1973@gmail.com>
2024-06-06 09:54:57 +0530

    Added 07

--- 06_string.js
+++ 06_string.js
@@ -9,3 +9,29 @@ let gameName = new String('Elden Ring'); // another way to declaring strings usi
 
 // now we must revise all the different methods of the String class
 
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
---
+++ 07_nums_and_math.js
@@ -0,0 +1,22 @@
+const score = 400; // javascript automatically declares it as a number
+//console.log(score);
+
+const balance = new Number(100);//explicitly defining it as a number
+//console.log(balance);
+
+//console.log(balance.toString()); //converts num => string
+
+// toFixed method quite frequently while making e-commerce websites
+// console.log(balance.toFixed(2));// => 100.00
+
+//toPrecision method gives value in significant numbers
+const anotherNum = 234.69;
+// console.log(anotherNum.toPrecision(4)); //=> 234.7
+
+const hunderds = 1000000;
+// console.log(hunderds.toLocaleString());
+// console.log(hunderds.toLocaleString('en-IN'));
+
+// **************************MATHS********************************
+
+console.log(Math);
